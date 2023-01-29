ad860a06743932b62090d625ec2dbdf3fd331379
“Aworefa” <aworefatobi1999@gmail.com>
2023-01-29 05:02:41 -0500

    new year

---
+++ nov25.js
@@ -0,0 +1,10 @@
+for (let i = 50; i < 60; i++) {
+    // console.log('good' , i);
+    console.log(i)
+}
+ var susu = ['glory', 'glo', 'dave']
+ for (let i = 0; i < susu.length; i++) {
+    console.log( i, susu[i])
+    
+ }
+ console.log(susu.length)
---
+++ oct19.js
@@ -0,0 +1,7 @@
+for (let i = 0; i <= 5; i++) {
+    console.log(`${2*i}`)  
+    for (let j = 20; j >= 0; j-=2) {
+        console.log(`${'good' + j }`)
+        
+    }
+}
